4bd83bef8e062d3b9941e49c50953748c4927ca2
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2023-08-09 00:20:44 -0300

    fix: identificando handlers list em superclasses

--- common/src/main/kotlin/me/sknz/minecraft/event/ListenerUtils.kt
+++ common/src/main/kotlin/me/sknz/minecraft/event/ListenerUtils.kt
@@ -7,28 +7,28 @@ import reactor.core.publisher.Mono
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KType
-import kotlin.reflect.full.companionObject
-import kotlin.reflect.full.companionObjectInstance
-import kotlin.reflect.full.declaredFunctions
-import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.*
 import kotlin.reflect.jvm.ExperimentalReflectionOnLambdas
 import kotlin.reflect.jvm.isAccessible
 import kotlin.reflect.jvm.reflect
 
 typealias ListenerExecutor<T> = (Mono<T>) -> Unit
 
+const val HANDLERS_FIELD = "getHandlerList"
+
 @OptIn(ExperimentalReflectionOnLambdas::class)
 fun <T : Event> ListenerExecutor<T>.getHandlerListFromExecutor(): HandlerList {
     val event = (this.reflect()!!.parameters[0].type.arguments[0].type!!.classifier as KClass<*>)
-    val getHandlerList = event.declaredFunctions.find { it.name == "getHandlerList" }
+    val getHandlerList = event.declaredFunctions.find { it.name == HANDLERS_FIELD }
         ?: throw IllegalArgumentException("Sem nenhum evento")
 
     return getHandlerList.call() as HandlerList
 }
 
 fun <T : Event> KClass<T>.getHandlerListFromEvent(): HandlerList {
-    val getHandlerList = this.declaredFunctions.find { it.name == "getHandlerList" }
-        ?: this.companionObject?.declaredFunctions?.find { it.name == "getHandlerList" }
+    val getHandlerList = this.declaredFunctions.find { it.name == HANDLERS_FIELD }
+        ?: this.companionObject?.declaredFunctions?.find { it.name == HANDLERS_FIELD }
+        ?: this.superclasses.firstNotNullOfOrNull { it.declaredFunctions.find { fn -> fn.name == HANDLERS_FIELD } }
         ?: throw IllegalArgumentException("Não foi encontrado nenhuma lista de manipulação (getHandlerList)")
 
     getHandlerList.isAccessible = true
