1288add15d2f8f539a5b68f30ec238f5222eb2a5
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2023-08-09 00:17:26 -0300

    refactor: atualizada a implementação do timer

--- party/src/main/kotlin/me/sknz/minecraft/party/events/GameTimer.kt
+++ party/src/main/kotlin/me/sknz/minecraft/party/events/GameTimer.kt
@@ -1,30 +1,77 @@
 package me.sknz.minecraft.party.events
 
 import me.sknz.minecraft.annotations.ExperimentalPluginFeature
+import me.sknz.minecraft.party.instance
+import reactor.core.Disposable
 import reactor.core.publisher.Flux
-import java.time.Duration
+import reactor.core.publisher.Sinks
+import java.util.concurrent.TimeUnit
 
 typealias GameTime = Long
 
 @ExperimentalPluginFeature
 class GameTimer(val initialTime: GameTime) {
 
-    private lateinit var _timer: Flux<GameTime>
+    private var task: Disposable? = null
 
-    lateinit var timer: Flux<GameTime>
-        private set
+    private val sinks = Sinks.many()
+        .multicast()
+        .onBackpressureBuffer<GameTime>()
+
+    val timer: Flux<GameTime>
+        get() = sinks.asFlux()
 
     var time: GameTime = initialTime
         private set
-
+    var isComplete: Boolean = false
+        private set
+    var isPaused: Boolean = false
+        private set
     var isStopped: Boolean = false
+        private set
 
-    fun start() {
-        _timer = Flux.interval(Duration.ofSeconds(1))
-            .takeUntil { it >= initialTime || isStopped }
-            .map { initialTime - it }
-            .doOnNext { time = it }
+    fun play() {
+        if (task != null) {
+            return
+        }
 
-        timer = _timer.share()
+        isPaused = false
+        task = instance.async.schedulePeriodically({
+            if (time == 0L) {
+                isComplete = true
+                sinks.tryEmitComplete()
+                task!!.dispose()
+                return@schedulePeriodically
+            }
+            sinks.tryEmitNext(time - 1)
+            time--
+        }, 0, 1, TimeUnit.SECONDS)
+    }
+
+    fun stop() {
+        if (isComplete) return
+        task?.dispose()
+        isStopped = true
+        sinks.tryEmitComplete()
+    }
+
+    fun end() {
+        if (isComplete || isStopped) return
+        task?.dispose()
+        time = 0
+        isComplete = true
+        sinks.tryEmitComplete()
+    }
+
+    fun pause() {
+        if (task == null) return
+        task!!.dispose()
+        task = null
+        isPaused = true
+    }
+
+    fun reset() {
+        if (isComplete || isStopped) return
+        time = initialTime
     }
 }
