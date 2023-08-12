c28269a4ba82f2c6a8ab0331a4544c1ddead8ef9
Cristian "SknZ" Ferreira <cristian.datingaa@gmail.com>
2023-08-11 23:46:05 -0300

    feature: agora o item de save retorna ao estado Starting Game

--- party/src/main/kotlin/me/sknz/minecraft/party/inventory/SaveConfigurationItemHandler.kt
+++ party/src/main/kotlin/me/sknz/minecraft/party/inventory/SaveConfigurationItemHandler.kt
@@ -3,6 +3,7 @@ package me.sknz.minecraft.party.inventory
 import me.sknz.minecraft.annotations.ExperimentalPluginFeature
 import me.sknz.minecraft.inventory.ItemHandler
 import me.sknz.minecraft.party.configurations.GameConfiguration
+import me.sknz.minecraft.party.events.GameStateChange
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -11,9 +12,16 @@ import org.bukkit.inventory.ItemStack
 @OptIn(ExperimentalPluginFeature::class)
 class SaveConfigurationItemHandler(val configuration: GameConfiguration)
     : ItemHandler(Material.BED, "Concluir as Configurações", RIGHT_CLICK) {
+
+    private var isSaved = false
+
     override fun onClick(player: Player, item: ItemStack, scope: ItemHandlerScope) {
+        if (isSaved) return
+
+        isSaved = true
         configuration.save()
         player.sendMessage("§aSuas configurações foram salvas com sucesso.")
+        Bukkit.getPluginManager().callEvent(GameStateChange(GameStateChange.GameState.STARTING))
     }
 
     override fun getItemCopy(): ItemStack {
